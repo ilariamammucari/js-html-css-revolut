ac8d291a24304d6b303061d926ff3db513460438
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-01-12 16:20:49 +0100

    add modify

--- js/script.js
+++ js/script.js
@@ -3,11 +3,8 @@ $(document).ready(function(){
   $('.drop-menu').hover(function(){
     $(this).children('.dropdown').slideToggle();
     
-    
-    if ( $('.drop-menu').index() == 4 ){
-      $('i').eq(0).toggleClass('active');
-      $('i').eq(1).toggleClass('active');
-    }
+    $(this).find('i').toggleClass('active')
+
   });
 
 });
