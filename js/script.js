9f8ff8300bf2c7f877f589bb96b458708a75aff1
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-01-12 16:40:56 +0100

    stesso esercizio ma .on click

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
 
-  $('.drop-menu').hover(function(){
+  // $('.drop-menu').hover(function(){
+  //   $(this).children('.dropdown').slideToggle();
+    
+  //   $(this).find('i').toggleClass('active');
+
+  // });
+
+
+  $('.drop-menu').on('click',function(){
     $(this).children('.dropdown').slideToggle();
     
-    $(this).find('i').toggleClass('active')
+    $(this).find('i').toggleClass('active');
 
   });
 
