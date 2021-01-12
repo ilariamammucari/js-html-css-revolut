0114ec271e5324c3db784d65275a05cf15b9be17
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-01-12 15:21:13 +0100

    on hover change chevron

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 
   $('.drop-menu').hover(function(){
     $(this).children('.dropdown').slideToggle();
-
+    
+    
+    if ( $('.drop-menu').index() == 4 ){
+      $('i').eq(0).toggleClass('active');
+      $('i').eq(1).toggleClass('active');
+    }
   });
-
-
-
-
-
 
 });
